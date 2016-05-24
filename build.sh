ae0fd4f89e16e6746baab8f628f6ed9b9884d4ff
Sam Creasey <sam.creasey@tri.global>
2016-05-24 14:36:36 -0400

    Override CFLAGS to keep clang from doing the wrong thing.

--- build.sh
+++ build.sh
@@ -18,5 +18,19 @@ for i in ThirdParty/*; do
     cd $ORIG_PWD
 done
 
+
+# IPOPT's build system sets -pedantic-errors when building with clang
+# on linux, which makes it's autoconf script fail tests which should
+# pass.  Sigh.
+if [ "$BUILD_TYPE" = "Debug" ]; then
+    CFLAGS="-g -O0"
+    CXXFLAGS="-g -O0"
+else
+    CFLAGS="-O3"
+    CXXFLAGS="-O3"
+fi
+export CFLAGS
+export CXXFLAGS
+
 ./configure  --with-blas=BUILD --with-lapack=BUILD --prefix=${CMAKE_INSTALL_PREFIX} --includedir=${CMAKE_INSTALL_PREFIX}/include/nlopt
 make install
