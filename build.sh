ddd394f8689fb514815c08b4be9df3c7aa0b8064
Sam Creasey <sam.creasey@tri.global>
2016-05-25 11:22:34 -0400

    Don't keep running the get.Foo scripts every pass through the superbuild.

--- build.sh
+++ build.sh
@@ -6,16 +6,28 @@ set -e
 
 CMAKE_INSTALL_PREFIX=$1
 
-svn checkout --non-interactive --trust-server-cert https://projects.coin-or.org/svn/Ipopt/stable/3.12 .
+IPOPT_VERSION=3.12
 
+svn checkout --non-interactive --trust-server-cert https://projects.coin-or.org/svn/Ipopt/stable/$IPOPT_VERSION .
+
+# IPOPT needs to download yet more code for third-party depenencies,
+# so do that now.
 ORIG_PWD=$PWD
 for i in ThirdParty/*; do
-    GET_SCRIPT_NAME=./get.`echo $i | cut -d / -f 2;`
+    THIRD_PARTY_LIB=`echo $i | cut -d / -f 2;`
+    echo Dowloading $THIRD_PARTY_LIB...
+    if [ -e downloaded.$THIRD_PARTY_LIB ]; then
+	echo Already downloaded $THIRD_PARTY_LIB
+	continue
+    fi
+    GET_SCRIPT_NAME=./get.$THIRD_PARTY_LIB
     cd $i
     if [ -x $GET_SCRIPT_NAME ]; then
 	$GET_SCRIPT_NAME
     fi
     cd $ORIG_PWD
+    echo Download of $THIRD_PARTY_LIB complete.
+    touch downloaded.$THIRD_PARTY_LIB
 done
 
 
